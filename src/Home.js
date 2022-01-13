d50c8030c4038896bb18df2ec051039319742efa
wqd182212 <10104911+wqd182212@user.noreply.gitee.com>
2022-01-13 16:48:32 +0800

    useReducer

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useContext, useRef, useReducer } from 'react';
 import { Button, Input } from 'antd';
 import './Home.css';
 
@@ -12,7 +12,19 @@ const theme = {
         background: '#1890ff',
     },
 };
-const ThemeContext = React.createContext(theme.b);
+const ThemeContext = React.createContext();
+
+const myReducer = (state, action) => {
+    switch(action.type) {
+        case "up":
+            return {
+                ...state,
+                newCount: state.newCount + 1,
+            }
+        default:
+            return state;
+    }
+}
 
 function Home() {
     useEffect(() => {
@@ -42,6 +54,7 @@ function ThemeButton() {
     const [count, setCount] = useState(0);
     const theme = useContext(ThemeContext);
     const inputElement = useRef(null);
+    const [state, dispatch] = useReducer(myReducer, { newCount: 100 });
     
     const handleClick = () => {
         setCount(count => count + 1);
@@ -58,6 +71,9 @@ function ThemeButton() {
                 {count}
             </Button>
             <Input ref={inputElement} style={{ width: 160 }}></Input>
+            <Button onClick={() => dispatch({ type: "up" })} style={{ margin: 10, color: theme.color, background: theme.background }}>
+                {state.newCount}
+            </Button>
         </div>
     );
 }
